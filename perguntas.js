a25983bb01e9ec32a619fb4eee413f9c21f5421e
kauanlide <kauan.januario.bento@escola.pr.gov.br>
2025-05-13 14:28:12 +0000

    ok

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    programação',
+    o que é html,
+    resposta
+)
+
+criaCartao(
+    ingles,
+    como é dia do pagamento,
+    resposta
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
